19674352f92f61638373e98f9e4af7e57962198b
otw03 <86701873+otw03@users.noreply.github.com>
2022-07-02 06:01:25 +0900

    Create 016백준 별 찍기 - 1.js

---
+++ 백준 코딩테스트 코드/016백준 별 찍기 - 1.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+let N = parseInt(input[0]);
+let result = "";
+let star = "*";
+
+for(let i=1; i<=N; i++){
+    let temp = "";
+    for(let j=1; j<=i; j++){
+        temp = temp + star;
+    }
+    //result = result + temp + "\n";
+    result = `${result+temp}\n`;
+}
+console.log(result);
+
