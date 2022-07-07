4f60f38ca0abea715d00e33e565515ab80236826
otw03 <86701873+otw03@users.noreply.github.com>
2022-07-08 01:39:11 +0900

    Create 021백준 더하기 사이클.js

---
+++ 백준 코딩테스트 코드/021백준 더하기 사이클.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().split('\n');
+
+let N = input[0];
+let i = 0;
+let A = N;  // while문 안에 있으면 실행할 때마다 A=N이 된다
+
+while(1){
+    i++;
+    let result = '';
+    let temp = 0;
+
+    if(A<10){
+        A = '0' + A;    // js에서 문자+숫자=문자
+    } else{
+        A = A.toString();   // A = parseInt(result) 때문에 숫자형인 A를 문자형으로 변환
+    }
+
+    temp = parseInt(A[0])+parseInt(A[1]);
+
+    if(temp<10){
+        temp = '0' + temp;
+    }else{
+        temp = temp.toString(); // temp = parseInt(A[0])+parseInt(A[1]) 때문에 숫자형인 temp를 문자형으로 변환
+    }
+
+    result = A[1]+temp[1];
+
+    A = parseInt(result);   //  A에 result정수형으로 반환, result가 00~09 일 때 방지, 안 하면 000~009로 될 수 있음
+    if( parseInt(N) == parseInt(result) ){ break; } // 반복문 종료
+}
+console.log(i);
