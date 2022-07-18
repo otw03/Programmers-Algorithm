08f0f0fc01f69b0012800f01ab80efd3635954e1
otw03 <86701873+otw03@users.noreply.github.com>
2022-07-18 21:15:54 +0900

    Update 025백준 나머지.js

--- 백준 코딩테스트 코드/025백준 나머지.js
+++ 백준 코딩테스트 코드/025백준 나머지.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const { tmpdir } = require("os");
 const input = fs.readFileSync("/dev/stdin").toString().split('\n');
 
 let A = 0;
@@ -14,3 +15,68 @@ let result = [...new Set(temp)];
 console.log(result.length);
 
 /* set() 사용하여 배열에서 중복 제거가 가능하다 */
+/* 
+// 2번째 풀이
+let A = 0;
+let temp = [];
+let count = 0;
+
+for(let i=0; i<10; i++)
+{
+    let A = input[i]%42;
+    temp.push(A);
+}
+for(let i=0; i<temp.length; i++){
+    for(let j=0; j<=i; j++){
+        if(i!=j && temp[i]==temp[j]){
+            count--;
+            console.log('count1');
+            break;
+        }
+    }
+    count++;
+    console.log('count2');
+}
+console.log(count);
+ */
+/* 
+// 2
+let A = 0;
+let temp = [];
+let result = 0;
+
+for(let i=0; i<10; i++)
+{
+    let A = input[i]%42;
+    temp.push(A);
+}
+for(let i=0; i<temp.length; i++){
+    let count = 0;
+    for(let j=0; j<i; j++){
+        if(temp[i]==temp[j]) { count++; }
+    }
+    if(count==0) { result++ }
+}
+console.log(result);
+ */
+
+/*
+// 3 
+let A = 0;
+let temp = [];
+let result = 0;
+
+for(let i=0; i<10; i++)
+{
+    let A = input[i]%42;
+    temp.push(A);
+}
+for(let i=0; i<temp.length; i++){
+    let count = 0;
+    for(let j=i+1; j<temp.length; j++){
+        if(temp[i]==temp[j]) { count++; }
+    }
+    if(count==0) { result++ }
+}
+console.log(result);
+ */
