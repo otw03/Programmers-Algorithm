4f0479958d2b4e71b5c7e49541a480453973eae6
otw03 <86701873+otw03@users.noreply.github.com>
2022-08-01 19:56:24 +0900

    028

--- 백준 코딩테스트 코드/028백준 평균은 넘겠지.js
+++ 백준 코딩테스트 코드/028백준 평균은 넘겠지.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const input = fs.readFileSync("input.txt").toString().split('\n');
+const input = fs.readFileSync("/dev/stdin").toString().split('\n');
 
 let C = input[0];
 
