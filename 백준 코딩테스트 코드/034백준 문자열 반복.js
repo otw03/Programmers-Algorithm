4021c91ac7844e08f45604c1f686e1f94afd0d63
otw03 <86701873+otw03@users.noreply.github.com>
2022-08-08 21:46:09 +0900

    백준034 문자열 반복

--- 백준 코딩테스트 코드/034백준 문자열 반복.js
+++ 백준 코딩테스트 코드/034백준 문자열 반복.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const input = fs.readFileSync("input.txt").toString().split('\n');
+const input = fs.readFileSync("/dev/stdin").toString().split('\n');
 
 let T = parseInt(input[0]);
 for(let i=1; i<=T; i++) {
