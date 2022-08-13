ef168dd5b69da62cd8a6098f02a7047f2a067011
otw03 <86701873+otw03@users.noreply.github.com>
2022-08-13 13:50:48 +0900

    백준035 단어공부

--- 백준 코딩테스트 코드/035백준 단어 공부.js
+++ 백준 코딩테스트 코드/035백준 단어 공부.js
@@ -1,14 +1,17 @@
 const fs = require("fs");
-const input = fs.readFileSync("input.txt").toString().split('\n');
+const input = fs.readFileSync("/dev/stdin").toString().split('\n');
 
-
+// 대문자로 통일
 let S = input[0].toUpperCase();
-
+// 빈 배열 만들고 0채워넣기
 let arr = new Array(26);
 arr.fill(0);
-console.log(arr);
+
 let max = 0;
 
+// A~Z : 아스키코드 숫자 65~90
+// 입력받은 문자를 숫자로 변환하고 -65 해서 배열의 index값에 맞춤
+// 각 문자가 몇번 사용됐는지, 제일 많이 사용된 횟수 구하기
 for(let i = 0; i < S.length; i++) {
     let num = S.charCodeAt(i)-65;
     arr[num]++;
@@ -17,19 +20,16 @@ for(let i = 0; i < S.length; i++) {
         }
 }
 
-console.log(arr);
-console.log(max);
-
 let result = "";
 
-
+// 가장 많이 사용된 문자 찾기, 여러개일 경우 ? 출력
 for(let i = 0; i < arr.length; i++) {
     if(arr[i] == max) {
         if(result == "") {
             result = String.fromCodePoint(i+65);
         } else {
             result = "?";
-            break;
+            break; // ? 나오면 더이상 실행할 필요 없으므로 끝내기
         }
     }
 }
