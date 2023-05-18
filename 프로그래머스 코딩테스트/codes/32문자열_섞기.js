b7a4fd3c082aa2c808dd066f08656b4042ad1c49
otw03 <86701873+otw03@users.noreply.github.com>
2023-05-18 23:59:36 +0900

    프로그래머스 문자열 섞기

---
+++ 프로그래머스 코딩테스트/codes/32문자열_섞기.js
@@ -0,0 +1,15 @@
+// function solution(str1, str2) {
+//     let answer = '';
+
+//     for(let i = 0; i < str1.length; i++) {
+//       answer += str1[i] + str2[i];
+//   }
+
+//     return answer;
+// }
+
+// 
+function solution(str1, str2) {
+    
+  return [...str1].map((v, i) => v + str2[i]).join('');
+}
